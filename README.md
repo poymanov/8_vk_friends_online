f1d177871a58780cfd50d8173fff060764032388
Nicolay Poymanov <n.poymanov@gmail.com>
2018-08-26 21:57:26 +0300

    add app logic

--- README.md
+++ README.md
@@ -1,17 +1,49 @@
 # Watcher of Friends Online
 
-[TODO. There will be project description]
+Скрипт отображает список друзей ВКонтакте, которые подключены к сети в данный момент.
 
-# How to Install
+# Предварительные настройки
 
-Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
-
-```bash
-pip install -r requirements.txt # alternatively try pip3
+- Создать конфигурационный файл приложения `.env`:
+```
+cp .env.example .env
+```
+- Создать [приложение](https://vk.com/editapp?act=create) ВКонтакте. (тип приложения - **standalone**)
+- Из настроек приложения ВК скопировать **ID приложения** и добавить в `.env`:
+```
+APP_ID=123456
+```
+- Установить и запустить [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) для Python
+- Установить дополнительные пакеты:
+```
+pip install -r requirements.txt
 ```
 
-Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.
+# Как запустить
 
-# Project Goals
+Скрипт требует для своей работы установленного интерпретатора **Python** версии **3.5**.
 
-The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+**Запуск на Linux**
+
+```bash
+$ python vk_friends_online # или python3, в зависимости от настроек системы
+
+# скрипт запросит логин/пароль VK для авторизации
+Enter your VK login:
+Enter your VK password:
+
+# при успешном выполнении отобразит список друзей, которые находятся в сети
+Users online:
+Иван Иванов
+Петр Петров
+Андрей Андреев
+
+# если введены неправиные логин/пароль/app_id выведет сообщение об ошибке
+Failed to connect to VK. Make sure you have entered the correct login/password.
+```
+
+Запуск на **Windows** происходит аналогично.
+
+# Цели проекта
+
+Код создан в учебных целях. В рамках учебного курса по веб-разработке - [DEVMAN.org](https://devman.org)
--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,32 +1,64 @@
 import vk
+import sys
+import os
+from getpass import getpass
+from dotenv import load_dotenv
 
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+def get_api_credentials():
+    load_dotenv(os.path.join(os.path.dirname(__name__), '.env'))
+    return {'app_id': os.getenv('APP_ID'), 'version': os.getenv('VERSION')}
 
 
 def get_user_login():
-    pass
+    login = input('Enter your VK login: ')
+    return login
 
 
 def get_user_password():
-    pass
+    password = getpass(prompt='Enter your VK password: ')
+    return password
 
 
-def get_online_friends(login, password):
-    session = vk.AuthSession(
-        app_id=APP_ID,
-        user_login=login,
-        user_password=password,
-    )
-    api = vk.API(session)
-    # например, api.friends.get()
+def get_api_session(login, password):
+    api_credentials = get_api_credentials()
+
+    try:
+        session = vk.AuthSession(
+            app_id=api_credentials['app_id'],
+            user_login=login,
+            user_password=password,
+            scope='friends'
+        )
+        return vk.API(session, version=api_credentials['version'])
+    except vk.exceptions.VkAuthError:
+        return None
+
+
+def get_online_friends(api_session):
+    friends_online_ids = api_session.friends.getOnline()
+    return api_session.users.get(user_ids=friends_online_ids)
 
 
 def output_friends_to_console(friends_online):
-    pass
+    print('Users online:')
+    for friend in friends_online:
+        print('{} {}'.format(friend['first_name'], friend['last_name']))
+
 
 if __name__ == '__main__':
     login = get_user_login()
     password = get_user_password()
-    friends_online = get_online_friends(login, password)
+
+    api_session = get_api_session(login, password)
+
+    if not api_session:
+        sys.exit('Failed to connect to VK. ' +
+                 'Make sure you have entered the correct login/password.')
+
+    friends_online = get_online_friends(api_session)
+
+    if not friends_online:
+        sys.exit('There are no users online')
+
     output_friends_to_console(friends_online)
